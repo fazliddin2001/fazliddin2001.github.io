cc86c46c6c5d1cfe4f96b6647d3f1a5c338c1b50
fazliddin2001 <Sardor19871115@mail.ru>
2020-12-22 14:03:25 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+/*** bootstrap carousel */
 var myCarousel = document.querySelector("#myCarousel");
 var carousel = new bootstrap.Carousel(myCarousel, {interval: 2000, slide: true, pause: false});
+
+
+/*** marquee */
+jQuery(document).ready(function($) {
+    $('.slick.marquee').slick({
+      speed: 5000,
+      autoplay: true,
+      autoplaySpeed: 0,
+      centerMode: true,
+      cssEase: 'linear',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      infinite: true,
+      initialSlide: 1,
+      arrows: false,
+      buttons: true
+    });
+  });
