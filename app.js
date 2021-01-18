ca14dbe88e5b015521a77e4d644a140521404cd6
fazliddin2001 <Sardor19871115@mail.ru>
2021-01-18 16:07:03 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -20,3 +20,99 @@ jQuery(document).ready(function($) {
       buttons: true
     });
   });
+
+/*$("a.nav-link").mouseover(function (){
+  if($(this).hasClass("news")){
+    $(".news-list").css({"height":"auto",  "display": "block"});
+  }else{
+    $(".news-list").css("height", "0px");
+  }
+  
+  if($(this).hasClass("infrastructure")){
+    $(".infrastructure-list").css({"height":"auto",  "display": "block"});
+  }else{
+    $(".infrastructure-list").css("height", "0px");
+  }
+
+  if($(this).hasClass("activity")){
+    $(".activity-list").css({"height":"auto",  "display": "block"});
+  }else{
+    $(".activity-list").css("height", "0px");
+  }
+
+  if($(this).hasClass("student")){
+    $(".student-list").css({"height":"auto",  "display": "block"});
+  }else{
+    $(".student-list").css("height", "0px");
+  }
+
+  if($(this).hasClass("class")){
+    $(".class-list").css({"height":"auto",  "display": "block"});
+  }else{
+    $(".class-list").css("height", "0px");
+  }
+
+});
+
+$("a.nav-link").mouseout(function (){
+
+  if($(this).hasClass("news")){
+    $(".news-list").css("height", "0px")
+    $(".news-list").bind("mouseover", function(){
+        $(".news-list").css({"height":"auto",  "display": "block"})
+      })
+    }
+    else{
+      $(".news-list").bind("mouseout", function(){
+        $(this).css("height", "0px");
+      })
+    }
+  
+  if($(this).hasClass("infrastructure")){
+    $(".infrastructure-list").css("height", "0px")
+    $(".infrastructure-list").bind("mouseover", function(){
+        $(".infrastructure-list").css({"height":"auto",  "display": "block"})
+      })
+    }
+    else{
+      $(".infrastructure-list").bind("mouseout", function(){
+        $(this).css("height", "0px");
+      })
+    }
+   
+  if($(this).hasClass("activity")){
+    $(".activity-list").css("height", "0px")
+    $(".activity-list").bind("mouseover", function(){
+        $(".activity-list").css({"height":"auto",  "display": "block"})
+      })
+    }
+    else{
+      $(".activity-list").bind("mouseout", function(){
+        $(this).css("height", "0px");
+      })
+    }
+
+    if($(this).hasClass("student")){
+      $(".student-list").css("height", "0px")
+      $(".student-list").bind("mouseover", function(){
+          $(".student-list").css({"height":"auto",  "display": "block"})
+        })
+      }
+      else{
+        $(".student-list").bind("mouseout", function(){
+          $(this).css("height", "0px");
+        })
+      }
+
+      if($(this).hasClass("class")){
+        $(".class-list").css("height", "0px")
+        $(".class-list").bind("mouseover", function(){
+            $(".class-list").css({"height":"auto",  "display": "block"})
+          })
+        }
+        else{
+          $(".class-list").bind("mouseout", function(){
+            $(this).css("height", "0px");
+          })
+        }
+});*/
